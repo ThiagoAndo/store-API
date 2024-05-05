edbd1620de39c6006189e8daedbac6da566f9266
ThiagoAndo <ando.thiago@gmail.com>
2024-05-05 19:56:09 +0100

    working on cart rout

--- actions/cartActions.js
+++ actions/cartActions.js
@@ -1,6 +1,6 @@
 const sql = require("better-sqlite3");
 const db = sql("e-comerce.db");
-const { updateAction } = require("../CRUD/actions");
+const { updateAction, createAction, readAction } = require("../CRUD/actions");
 
 function getCart(query, queryVal) {
   const cart = db
@@ -10,19 +10,32 @@ function getCart(query, queryVal) {
 }
 
 function checkInsertCart({ user_id, item_id, qnt, price, name, creation_at }) {
-  const [product] = getCart(["item_id", "bought"], [item_id, 0]);
+  let ret;
+  let ret2;
+  const [product] = readAction("cart", "user_id=? AND bought=? AND item_id=?", [
+    user_id,
+    0,
+    item_id,
+  ]);
   if (product) {
-    updateAction("cart", "qnt = ?", "item_id = ?", [qnt, item_id]);
+    ret = updateAction("cart", "qnt = ?", "item_id = ? AND user_id=? ", [
+      qnt,
+      item_id,
+      user_id,
+    ]);
   } else {
-    insertCart({
+    ret2 = createAction("cart", {
       user_id,
       item_id,
       qnt,
+      bought: 0,
       price,
       name,
       creation_at,
     });
   }
+  console.log(ret);
+  return ret;
 }
 
 function updateCart(query, queryVal) {
---
+++ routes/cart.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { deleteAction, readAction } = require("../CRUD/actions");
+const { checkInsertCart } = require("../actions/cartActions");
+const router = express.Router();
+
+router.get("/:id", async (req, res) => {
+  const user_id = req.params.id;
+  const items = readAction("cart", "user_id=? AND bought=?", [user_id, 0]);
+
+  res.json({ items });
+});
+
+router.post("/", async (req, res) => {
+  const { items, id: user_id } = req.body;
+  let ret;
+  if (items.length === 0) {
+    ret = deleteAction("cart", " user_id = ? AND bought = ?", [user_id, 0]);
+    ret.changes > 0
+      ? res.status(200).json({ message: "Cart deleted" })
+      : res.status(404);
+  } else {
+    items.forEach((item) => {
+      const {
+        id: item_id,
+        quantity: qnt,
+        createAt: creation_at,
+        price,
+        name,
+      } = item;
+      ret = checkInsertCart({
+        user_id,
+        item_id,
+        qnt,
+        creation_at,
+        name,
+        price,
+      });
+    });
+    res.status(200).json({ message: "Cart created successufuly" });
+  }
+});
+
+module.exports = router;
