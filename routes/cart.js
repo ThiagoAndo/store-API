3f2261730ac643bc244b519e38030f37c2818d17
ThiagoAndo <ando.thiago@gmail.com>
2024-05-05 20:54:24 +0100

    cart actions OK

--- routes/cart.js
+++ routes/cart.js
@@ -3,12 +3,23 @@ const { deleteAction, readAction } = require("../CRUD/actions");
 const { checkInsertCart } = require("../actions/cartActions");
 const router = express.Router();
 
+router.get("/ordered/:id", async (req, res) => {
+  const user_id = req.params.id;
+  const items = readAction("cart", "user_id=? AND bought=?", [user_id, 1]);
+
+  res.json({ items });
+});
+
 router.get("/:id", async (req, res) => {
   const user_id = req.params.id;
+
   const items = readAction("cart", "user_id=? AND bought=?", [user_id, 0]);
 
   res.json({ items });
 });
+
+
+
 
 router.post("/", async (req, res) => {
   const { items, id: user_id } = req.body;
