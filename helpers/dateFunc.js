558c07094f30ab27635f58818bfccfa567e8f04d
ThiagoAndo <ando.thiago@gmail.com>
2024-05-08 12:14:46 +0100

    dateFunc.js OK

---
+++ helpers/dateFunc.js
@@ -0,0 +1,13 @@
+function getCurrentDate() {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  let currentDate = `${Number(day) < 10 ? 0 : ""}${day}-${
+    Number(month) < 10 ? 0 : ""
+  }${month}-${year}  ${new String(new Date(Date.now())).slice(15, 24)}`;
+
+  return currentDate;
+}
+exports.getCurrentDate = getCurrentDate;
