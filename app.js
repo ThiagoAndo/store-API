a7bd47daa0d8a4b0c6e7f2e9672adb7aac430f49
ThiagoAndo <ando.thiago@gmail.com>
2024-07-27 14:33:02 +0100

    Changed access control

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.engine("html", require("ejs").renderFile);
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
-  // res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
